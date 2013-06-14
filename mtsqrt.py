5f6b3f6967a3b2acc89656268d64fa5bb0b75617
uwhpsc <uwhpsc@ubuntu.(none)>
2013-06-14 04:13:40 -0700

    even better sqrt function

--- mtsqrt.py
+++ mtsqrt.py
@@ -1,6 +1,10 @@
-def mtsqrt(x, kmax):
-    s = 1
+def mtsqrt(x, s, kmax):
+    tol = 1.e-20
     for k in range(kmax):
     	print "Before iteration %s, s = %s" %(k,s)
+    	s0 = s
         s = 0.5 * (s + x/s)
+        delta_s = s - s0
+        if abs(delta_s/x) < tol:
+        	break
     print "After %s iterations, s = %s" %(k+1,s)
