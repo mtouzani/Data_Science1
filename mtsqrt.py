33298dd5861e4d903c36b310ede7ae6b3e776bd7
uwhpsc <uwhpsc@ubuntu.(none)>
2013-06-14 04:17:52 -0700

    added formating to script

--- mtsqrt.py
+++ mtsqrt.py
@@ -1,10 +1,10 @@
 def mtsqrt(x, s, kmax):
     tol = 1.e-20
     for k in range(kmax):
-    	print "Before iteration %s, s = %s" %(k,s)
+    	print "Before iteration %s, s = %20.15f" %(k,s)
     	s0 = s
         s = 0.5 * (s + x/s)
         delta_s = s - s0
         if abs(delta_s/x) < tol:
         	break
-    print "After %s iterations, s = %s" %(k+1,s)
+    print "After %s iterations, s = %20.15f" %(k+1,s)
