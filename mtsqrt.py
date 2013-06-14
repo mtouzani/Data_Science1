4289074e8d3a3e6ba329eb455cc68e45231b91b7
uwhpsc <uwhpsc@ubuntu.(none)>
2013-06-14 03:50:50 -0700

    better script

--- mtsqrt.py
+++ mtsqrt.py
@@ -1,6 +1,6 @@
 def mtsqrt(x, kmax):
     s = 1
     for k in range(kmax):
-    	print "Before iteration %s , s = %s" %(k,s)
+    	print "Before iteration %s, s = %s" %(k,s)
         s = 0.5 * (s + x/s)
-    print "After iteration %s , s = %s" %(k,s)
+    print "After %s iterations, s = %s" %(k+1,s)
